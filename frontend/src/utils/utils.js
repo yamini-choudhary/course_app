92d5fe74a1383c8c71c1b09479d6a0e400d59d94
Yamini Choudhary <yaminichoudhary29091998@gmail.com>
2025-06-23 22:51:20 +0530

    url changed

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://course-app-cwpc.onrender.com";
-export const BACKEND_URL = "http://localhost:4001/api/v1";
+export const BACKEND_URL = "https://course-app-cwpc.onrender.com/api/v1";
+// export const BACKEND_URL = "http://localhost:4001/api/v1";
