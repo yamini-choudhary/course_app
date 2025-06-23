f7b3b2bfbc2f67bcd6e7ed95cf66409a79df62cc
Yamini Choudhary <yaminichoudhary29091998@gmail.com>
2025-06-23 08:20:26 +0530

    app completed

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,7 @@ dotenv.config();
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY =
-  "sk_test_51RbMv2CNG7aGT0lVSpZueH6g1BrTKsfDaGfouhKbQroNS9oyYEMUJx0vQ4Wr8NeV3Tl36TmMfqbu0k29jnSjQfUg00sQr0cDqo";
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 export default {
   JWT_USER_PASSWORD,
